64b9986065a71ef2efe5e2b3ea655ad3c30411b2
Zazil Gomz <zazilgomez@Zazils-MacBook-Air.local>
2023-02-09 15:27:52 -0800

    rename

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeroutes/homeRoutes')
+const homeRoutes = require('./homeRoutes/homeRoutes')
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes)
